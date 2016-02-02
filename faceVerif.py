173f233bd03d7f678f73935af8b957d1d4ee607e
Ubuntu <ubuntu@ip-172-31-21-93.us-west-2.compute.internal>
2016-02-02 15:26:34 +0000

    server completed

--- faceVerif.py
+++ faceVerif.py
@@ -3,6 +3,8 @@ import tornado.ioloop
 import tornado.web
 import os, uuid
 import openface
+import cv2
+import numpy as np
 
 __UPLOADS__ = "uploads/"
 
@@ -25,7 +27,6 @@ class Upload(tornado.web.RequestHandler):
         fh2 = open(__UPLOADS__ + cname2, 'w')
         fh1.write(fileinfo1['body'])
         fh2.write(fileinfo2['body'])
-        self.finish("<img src="+cname1 +" /><img src="+cname2+"/>")
 
 
 	openfaceDir = os.path.join('..', 'openface')
@@ -33,9 +34,28 @@ class Upload(tornado.web.RequestHandler):
 	dlibModelDir = os.path.join(modelDir, 'dlib')
 	openfaceModelDir = os.path.join(modelDir, 'openface')
 	dlibFacePredictor = os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat")
-	networkModel = os.path.join(openfaceModelDir, 'nn4.v2.t7')
+	networkModel = os.path.join(openfaceModelDir, 'nn4.small2.v1.t7')
 	align =openface.AlignDlib(dlibFacePredictor)
 	net = openface.TorchNeuralNet(networkModel, 96)
+
+	bgrImg1 = cv2.imread(__UPLOADS__+cname1)
+	bgrImg2 = cv2.imread(__UPLOADS__+cname2)
+
+	rgbImg1 = cv2.cvtColor(bgrImg1, cv2.COLOR_BGR2RGB)
+	rgbImg2 = cv2.cvtColor(bgrImg2, cv2.COLOR_BGR2RGB)
+
+	bb1 = align.getLargestFaceBoundingBox(rgbImg1)
+	bb2 = align.getLargestFaceBoundingBox(rgbImg2)
+	
+	alignedFace1 = align.align(96, rgbImg1, bb1, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+	alignedFace2 = align.align(96, rgbImg2, bb2, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
+	rep1 = net.forward(alignedFace1)
+	rep2 = net.forward(alignedFace2)
+
+	d = rep1 - rep2
+	D = np.dot(d,d)
+        self.finish("<img src="+cname1 +" /><img src="+cname2+"/>"+str(D))
 
 
 application = tornado.web.Application([
